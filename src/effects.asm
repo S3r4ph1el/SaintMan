3255ce7a2165c95676128e65f0ea08d3ae3fb995
Victor Martinez <victoryanmartinez@gmail.com>
2024-09-18 23:05:22 -0300

    feat: endgame song

--- src/effects.asm
+++ src/effects.asm
@@ -5,6 +5,9 @@ GAMEOVERENDTIME: .word 0
 DEATHLENGTH: .word 6
 DEATHNOTES: .word 66,152,67,152,66,152,67,152,67,304,55,304
 DEATHENDTIME: .word 0
+ENDGAMELENGTH: .word 9
+ENDGAMENOTES: .word 84,110,84,110,84,110,84,331,82,331,83,331,84,221,83,110,84,331
+ENDGAMEENDTIME: .word 0
 
 .text
 
@@ -69,6 +72,30 @@ DEATHSTART:	beq s10,s9, START_MAIN
                 ecall
                 add s7, s7, a0
                 sw s7, DEATHENDTIME, a7
+                addi s11,s11,8			
+                addi s10,s10,1
+                ret
+ENDGAMESETUP:	la s11,ENDGAMELENGTH			
+                lw s9,0(s11)
+                la s11,ENDGAMENOTES
+                li s10,0		
+ENDGAMEPLAY:	beq s10, zero, ENDGAMESTART		
+                li a7, 30
+                ecall
+                lw s7, ENDGAMEENDTIME
+                blt a0, s7, RETURN1
+ENDGAMESTART:	beq s10,s9, RETURN1		
+                li a2, 1
+                li a3, 100	
+                lw a0,0(s11)		
+                lw a1,4(s11)		
+                li a7,31
+                ecall
+                mv s7, a1
+                li a7, 30
+                ecall
+                add s7, s7, a0
+                sw s7, ENDGAMEENDTIME, a7
                 addi s11,s11,8			
                 addi s10,s10,1
                 ret
